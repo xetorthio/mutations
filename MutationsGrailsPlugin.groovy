e396f9eebb607443c50744ce2c1331e5b06a4e76
Jonathan Leibiusky <jonathan.leibiusky@mercadolibre.com>
2010-04-30 14:45:49 -0300

    Added more info to the grails plugin info file

--- MutationsGrailsPlugin.groovy
+++ MutationsGrailsPlugin.groovy
@@ -5,4 +5,6 @@ class MutationsGrailsPlugin {
     def author = "Jonathan Leibiusky"
     def authorEmail = "ionathan@gmail.com"
     def description = "Grails Mutations plugin is a simple solution to the problem of managing incremental changes to the databases over time. It is called Mutation because there are a lot of Migration framework out there and it seems more accurate to say that the DB is being mutated. Each mutation is versioned and tracked in the database itself, so it is easy to detect when it need to be applied."
+    def documentation = "http://github.com/xetorthio/mutations"
+    def loadAfter = ['core', 'hibernate']
 }
